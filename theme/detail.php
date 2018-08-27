8b40606ba9c83eff0311782c955cfbffc75900b0
hieutct <hieutct@solazu.com>
2018-08-28 01:08:12 +0700

    add code

--- theme/detail.php
+++ theme/detail.php
@@ -6,58 +6,33 @@
         </h4>
     </div>
     <div class="session">
-        <h3 class="title-line"><i class="fa fa-lightbulb-o" aria-hidden="true" style="padding: 10px 15px"></i>Education</h3>
-        <div class="row">
-            <div class="col-md-6">
-                <h4><strong>University of Science</strong></h4>
-                <h4><strong>Major</strong> Software Engineering</h4>
-                <h4><strong>GPA</strong> 7.4/10</h4>
-            </div>
-            <div class="col-md-6">
-                <div class="text-right"><strong>2013 - 2017</strong></div>
-            </div>
-        </div>
-    </div>
-
-    <div class="session">
         <h3 class="title-line"><i class="fa fa-users" aria-hidden="true"></i>Work Experience</h3>
         <div class="row">
             <div class="col-md-6">
-                <h4><strong>Project In School</strong></h4>
-                <h4>Student</h4>
-                <h4><strong>Job description: </strong>
-                    <br><br>Completing projects assigned in subjects
-                </h4>
-            </div>
-            <div class="col-md-6">
-                <div class="text-right"><strong>2013 - 2017</strong></div>
-            </div>
-        </div>
-        <div class="line"></div>
-        <div class="row">
-            <div class="col-md-6">
-                <h4><strong>Prudential Finance</strong></h4>
-                <h4>Staff</h4>
-                <h4><strong>Job description: </strong>
-                    <br><br>&nbsp;&nbsp; 1. Build and SEO website.
-                    <br><br>&nbsp;&nbsp; 2. Design idea for event.
-                    <br><br>&nbsp;&nbsp; 3. Filter data.
-                </h4>
-            </div>
-            <div class="col-md-6">
-                <div class="text-right"><strong>2016</strong></div>
-            </div>
-        </div>
-        <div class="line"></div>
-        <div class="row">
-            <div class="col-md-6">
-                <h4><strong>Microsite + The Management System</strong></h4>
+                <h4><strong>Manager System</strong></h4>
                 <h4>Freelance</h4>
-                <h4><strong>Job description: </strong><br><br>Develop functionality for the system according to customer requirements</h4>
-                <h4><strong>Language: </strong>PHP</h4>
+                <h4>Yii / Angular / Bootstrap</h4>
+                 <h4><strong>Job description: </strong>
+                    <br><br> building management system for NhiLong, Extrim, Hoang Anh, Sabaidee, ...
+                </h4>
             </div>
             <div class="col-md-6">
-                <div class="text-right"><strong>07/2016 - Now</strong></div>
+                <div class="text-right"><strong>2015 - 2018</strong></div>
+            </div>
+        </div>
+        <div class="line"></div>
+        <div class="row">
+            <div class="col-md-6">
+                <h4><strong>Microsite</strong></h4>
+                <h4>Freelance</h4>
+                <h4>Wordpress</h4>
+                <h4>
+                    <strong>Job description: </strong><br><br>
+                    Products development according to customer requirements
+                </h4>
+            </div>
+            <div class="col-md-6">
+                <div class="text-right"><strong>2016 - 2018</strong></div>
             </div>
         </div>
         <div class="line"></div>
@@ -69,26 +44,13 @@
                     <br><br>1. Build product on framework wordpress. (PHP)
                     <br><br>2. Outsourcing. (PHP + Yii)
                     <br><br>3. BlockChain (NodeJs + ReactJs).
+                    <br><br>4. Hoteljp (Ruby + BigData + Firebase + ReactJS).
             </div>
             <div class="col-md-6">
-                <div class="text-right"><strong>11/2016 - Now</strong></div>
+                <div class="text-right"><strong>2016 - 2018</strong></div>
             </div>
         </div>
     </div>
-
-<!--    <div class="session">-->
-<!--        <h3 class="title-line"><i class="fa fa-certificate" aria-hidden="true"></i>Certifications</h3>-->
-<!--        <div class="row">-->
-<!--            <div class="col-md-6">-->
-<!--                <h4><strong>University of Science</strong></h4>-->
-<!--                <h4><strong>Major:</strong> Software Engineering</h4>-->
-<!--                <h4><strong>GPA</strong> 7.4/10</h4>-->
-<!--            </div>-->
-<!--            <div class="col-md-6">-->
-<!--                <div class="text-right"><strong>2013 - 2017</strong></div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
     <p class="quote">To succeed in life, you need two things: ignorance and confidence.<br><span class="author">Mark Twain</span>
     </p>
 </div>
--- theme/profile.php
+++ theme/profile.php
@@ -13,104 +13,45 @@
                 </li>
                 <li style="position: relative">
                     <i class="fa fa-location-arrow" aria-hidden="true" style="top: 6px;position: absolute;"></i>
-                    <h4 style="padding-left: 60px;">606/49/11, Đường 3/2, Phường 14, Quận 10, TP. Hồ Chí Minh</h4>
+                    <h4 style="padding-left: 60px;">606/49/11, 3/2 Street, Ward 14, District 10, Ho Chi Minh City. Ho Chi Minh</h4>
                 </li>
             </ul>
         </div>
-        <div class="session">
+         <div class="session">
             <h3 class="title-line">Skills</h3>
-            <div class="content">
+            <ul>
+               <!--            item-->
+                <li class="item-skill">
+                    <h4>Wordpress, Yii, Lavarel</h4>
+                </li>
                 <!--            item-->
-                <div class="item-skill">
-                    <h4>PHP (Wordpress, Lavarel, Yii)</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="70"
-                             aria-valuemin="0" aria-valuemax="100" style="width:85%">
-                            <span class="sr-only">85% Complete</span>
-                        </div>
-                    </div>
-                </div>
-                <!--            item-->
-                <div class="item-skill">
-                    <h4>Jquery/ Bootstrap/ Angular / React JS</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="70"
-                             aria-valuemin="0" aria-valuemax="100" style="width:90%">
-                            <span class="sr-only">70% Complete</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="item-skill">
-                    <h4>MYSQL/ SQL/ PostgreSql</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="80"
-                             aria-valuemin="0" aria-valuemax="100" style="width:80%">
-                            <span class="sr-only">80% Complete</span>
-                        </div>
-                    </div>
-                </div>
+                <li class="item-skill">
+                    <h4>Jquery, Angular, React JS</h4>
+                </li>
+                <li class="item-skill">
+                    <h4>MYSQL, SQL, PostgreSql, Firebase</h4>
+                </li>
                  <!--            item-->
-                <div class="item-skill">
-                    <h4>Python (Django)</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="40"
-                             aria-valuemin="0" aria-valuemax="100" style="width:55%">
-                            <span class="sr-only">40% Complete</span>
-                        </div>
-                    </div>
-                </div>
+                <li class="item-skill">
+                    <h4>Ruby</h4>
+                </li>
                 <!--            item-->
-                <div class="item-skill">
-                    <h4>NodeJs (Express), Java (Struts - Hibernate - Integration)</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="70"
-                             aria-valuemin="0" aria-valuemax="100" style="width:60%">
-                            <span class="sr-only">70% Complete</span>
-                        </div>
-                    </div>
-                </div>
+                <li class="item-skill">
+                    <h4>NodeJs, Java</h4>
+                </li>
                 <!--            item-->
-                <div class="item-skill">
-                    <h4>C#/ C++/ C</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="50"
-                             aria-valuemin="0" aria-valuemax="100" style="width:50%">
-                            <span class="sr-only">50% Complete</span>
-                        </div>
-                    </div>
-                </div>
+                <li class="item-skill">
+                    <h4>Git, SVN, Lampp, Ubuntu, ...</h4>
+                </li>
 
-                <div class="item-skill">
-                    <h4>Teamwork</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="85"
-                             aria-valuemin="0" aria-valuemax="100" style="width:85%">
-                            <span class="sr-only">85% Complete</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="item-skill">
-                    <h4>Tools(Git, SVN, Xampp, Ubuntu, ...)</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="95"
-                             aria-valuemin="0" aria-valuemax="100" style="width:90%">
-                            <span class="sr-only">95% Complete</span>
-                        </div>
-                    </div>
-                </div>
-
-                 <div class="item-skill">
+                 <li class="item-skill">
                     <h4>Environment(Ubuntu, Windows, ...)</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="95"
-                             aria-valuemin="0" aria-valuemax="100" style="width:70%">
-                            <span class="sr-only">95% Complete</span>
-                        </div>
-                    </div>
-                </div>
+                </li>
 
-            </div>
+                 <li class="item-skill">
+                    <h4>Redis, ElasticSearch, sidekiq</h4>
+                </li>
+            </ul>
         </div>
         <div class="session">
             <h3 class="title-line">Interests</h3>
