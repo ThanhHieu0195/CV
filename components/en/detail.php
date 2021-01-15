ed3a35e239408edf6a8e24f58b4edcb265a79bf2
Karrot <hieu@espxmedia.com>
2021-01-15 17:09:24 +0700

    update

--- components/en/detail.php
+++ components/en/detail.php
@@ -28,15 +28,13 @@ Also, I went through many position (frontend, backend, devops) so an overview of
         </h3>
 
         <div class="row">
-            <div class="col-md-10">
+            <div class="col-md-9">
                 <h4 class="item-title">ESPXMedia</h4>
                 <div class="wrap-block">
                     <h4><b>Platform</b></h4>
                     - FeathersJs, ReactJs/AngularJs, Postgres, Mongodb, Redis, Socket<br>
                     - AWS, DO <br>
                     - Bunny, CloudFront, CloudFlare <br>
-                    <h4><b>Company</b></h4>
-                    ESPXMedia 
                     <h4><b>Position:</b> </h4>
                     Technical Lead
 
@@ -58,21 +56,19 @@ Also, I went through many position (frontend, backend, devops) so an overview of
 
                 </div>
             </div>
-            <div class="col-md-2">
-                <div class="text-right"><strong>2019-2021</strong></div>
+            <div class="col-md-3">
+                <div class="text-right"><strong>10/2019 - 2021</strong></div>
             </div>
         </div>
         <div class="line"></div>
 
           <div class="row">
-            <div class="col-md-10">
+            <div class="col-md-9">
                 <h4 class="item-title">TECHVSI</h4>
                 <div class="wrap-block">
                     <h4><b>Platform</b></h4>
                     - Wordpress, Express, Mariadb <br>
                     - WPVIP, AWS <br>
-                    <h4><b>Company</b></h4>
-                    Techvsi  
                     <h4><b>Position:</b> </h4>
                     Technical Lead
 
@@ -85,38 +81,29 @@ Also, I went through many position (frontend, backend, devops) so an overview of
                     <a href="https://techvsi.com/">https://techvsi.com/</a>
                 </div>
             </div>
-            <div class="col-md-2">
-                <div class="text-right"><strong>2019-2021</strong></div>
-            </div>
-        </div>
-        <div class="line"></div>
-        <div class="row">
-            <div class="col-md-10">
-                <h4 class="item-title">Cralwer Service</h4>
-                <div class="wrap-block">
-                    <h4><b>Platform</b></h4>
-                    Python, Socket, Redis, Mongodb, Selenium, Queue
-                    <h4><b>Company</b></h4>
-                    Viralworks
-                    <h4><b>Job description: </b></h4>
-                    - Collect post information every day from social networks (facebook, instagram, youtube)<br>
-                    - Crawl user's information <br>
-                    - Screenshot post
-                </div>
-            </div>
-            <div class="col-md-2">
-                <div class="text-right"><strong>In 2019</strong></div>
+            <div class="col-md-3">
+                <div class="text-right"><strong>10/2019 - 2021</strong></div>
             </div>
         </div>
         <div class="line"></div>
 
         <div class="row">
-            <div class="col-md-10">
-                <h4 class="item-title">Platform KOL</h4>
+            <div class="col-md-9">
+                <h4 class="item-title">Viralworks</h4>
                 <div class="wrap-block">
+                    <h4 style="font-size: 20px"><b>Project</b></h4>
+                    Cralwer Service
                     <h4><b>Platform</b></h4>
-                    <h4><b>Company</b></h4>
-                    Viralworks
+                    Python, Socket, Redis, Mongodb, Selenium, Queue
+                    <h4><b>Job description: </b></h4>
+                    - Collect post information every day from social networks (facebook, instagram, youtube)<br>
+                    - Crawl user's information <br>
+                    - Screenshot post
+                </div>
+                <div class="wrap-block">
+                    <h4 style="font-size: 20px"><b>Project</b></h4>
+                    Platform KOL
+                    <h4><b>Platform</b></h4>
                     Nodejs, Laravel, Mongodb, Redis
                     <h4><b>Job description: </b></h4>
                     - The platform supports connecting KOL and Brand together<br>
@@ -124,22 +111,24 @@ Also, I went through many position (frontend, backend, devops) so an overview of
                     - Manage KOL, BRAND, ADMIN<br>
                     - Manage campaign
                 </div>
+
             </div>
-            <div class="col-md-2">
-                <div class="text-right"><strong>In 2018</strong></div>
+            <div class="col-md-3">
+                <div class="text-right"><strong>07/2018 - 10/2019</strong></div>
             </div>
         </div>
+
         <div class="line"></div>
 
         <div class="row">
-            <div class="col-md-10">
-                <h4 class="item-title">Hotel search system</h4>
+            <div class="col-md-9">
+                <h4 class="item-title">Solazu</h4>
 
                 <div class="wrap-block">
+                    <h4 style="font-size: 20px"><b>Project</b></h4>
+                    Hotel search system
                     <h4><b>Platform</b></h4>
                     Ruby, Redis, Firebase, ReactJS, ElasticSearch, Sidekiq, MySql
-                    <h4><b>Company</b></h4>
-                    Solazu
                     <h4><b>Job description: </b></h4>
                     - 3rd hotel search system of Japan<br>
                     - Crawl data from 32 OTA service when search<br>
@@ -147,46 +136,35 @@ Also, I went through many position (frontend, backend, devops) so an overview of
                     - Minimize latency<br>
                     (<a href="a">https://www.travel.co.jp/</a>)
                 </div>
-            </div>
-            <div class="col-md-2">
-                <div class="text-right"><strong>In 2018</strong></div>
-            </div>
-        </div>
-        <div class="line"></div>
-
-        <div class="row">
-            <div class="col-md-10">
-                <h4 class="item-title">Themes in themeforest</h4>
-                 <div class="wrap-block">
+                <div class="wrap-block">
+                    <h4 style="font-size: 20px"><b>Project</b></h4>
+                    Themes in themeforest
                      <h4><b>Platform</b></h4>
                      CMS Wordpress
-                     <h4><b>Company</b></h4>
-                    Solazu
                      <h4><b>Job description:</b></h4>
                      - Building themes, plugins, Shortcodes, ...<br>
                      - Major topics: education, music, hearty, events, ... <br>
                      (<a href="">https://themeforest.net/user/therubikthemes</a>)
                  </div>
+
             </div>
-            <div class="col-md-2">
+            <div class="col-md-3">
                 <div class="text-right"><strong>2016 - 2018</strong></div>
             </div>
         </div>
         <div class="line"></div>
 
         <div class="row">
-            <div class="col-md-10">
-                <h4 class="item-title">Management System</h4>
+            <div class="col-md-9">
+                <h4 class="item-title">Freelancer</h4>
                 <div class="wrap-block">
                     <h4><b>Platform</b></h4>
                     Wordpress, Laravel, Yii, Firebase
-                    <h4><b>Company</b></h4>
-                    Freelance
                     <h4><b>Job description:</b></h4>
                     - Building management system for Nhi Long, Extrim, Hoang Anh, Sabaidee, Techvsi, Nhat Quang, Nhan Hoa, ...
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-3">
                 <div class="text-right"><strong>2015 - 2017</strong></div>
             </div>
         </div>
